e9d7c64754b6c9ce9cc55dc56011a4f6b59f3443
ivalhad <ivan14hadrian@gmail.com>
2025-09-21 17:54:01 +0700

    update model to do

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 
-// Mendefinisikan skema (blueprint) untuk data to-do
 const todoSchema = new mongoose.Schema({
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User' 
+  },
   text: {
     type: String,
-    required: true, // Teks to-do wajib ada
+    required: [true, "Teks tidak boleh kosong"]
   },
   completed: {
     type: Boolean,
-    default: false, // Secara default, to-do belum selesai
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now, // Waktu pembuatan to-do
-  },
+    default: false
+  }
+}, {
+  timestamps: true 
 });
 
-// Membuat model dari skema, yang akan merepresentasikan koleksi 'todos' di MongoDB
 const Todo = mongoose.model('Todo', todoSchema);
 
 module.exports = Todo;
