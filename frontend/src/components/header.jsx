d129fdd57f026fe54cd26fe72a261ed0682f8e67
ivalhad <ivan14hadrian@gmail.com>
2025-09-21 23:04:11 +0700

    implement header

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+import Button from './button';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  const name = localStorage.getItem('name');
+
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('name');
+    navigate('/login');
+  };
+
+  return (
+    <header className="bg-primary text-white shadow-md">
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">To-Do List</h1>
+        {token && (
+           <div className="flex items-center space-x-4">
+           <span className="hidden sm:block">Halo, {name}!</span>
+           <Button onClick={handleLogout} secondary>
+             Logout
+           </Button>
+         </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
