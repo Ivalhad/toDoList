d0c64de6760a17bbcc307b2489424dc4447a04bc
ivalhad <ivan14hadrian@gmail.com>
2025-09-21 17:38:29 +0700

    update user model

---
+++ backend/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Nama tidak boleh kosong'],
+    unique: true, 
+    trim: true,
+    maxlength: [50, 'Nama tidak boleh lebih dari 50 karakter']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password tidak boleh kosong'],
+    minlength: [6, 'Password minimal harus 6 karakter'],
+    maxlength: [50, 'Password tidak boleh lebih dari 50 karakter']
+  }
+}, { timestamps: true });
+
+
+userSchema.pre('save', async function(next) {
+ 
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
