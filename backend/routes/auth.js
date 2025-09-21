9dc9ea785e2a2739a3a5a19799de3354da1442ae
ivalhad <ivan14hadrian@gmail.com>
2025-09-21 17:48:58 +0700

    update routes auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const InvalidToken = require('../models/invalidToken');
+const { protect } = require('../middleware/authMiddleware'); 
 
 // Fungsi untuk generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d', // Token akan kadaluarsa dalam 30 hari
+    expiresIn: '30d', 
   });
 };
 
@@ -14,7 +16,6 @@ router.post('/register', async (req, res) => {
   const { name, password } = req.body;
 
   try {
-    // Cek apakah user sudah ada
     const userExists = await User.findOne({ name });
     if (userExists) {
       return res.status(400).json({ message: 'Nama user sudah digunakan' });
@@ -39,7 +40,6 @@ router.post('/register', async (req, res) => {
   }
 });
 
-
 router.post('/login', async (req, res) => {
   const { name, password } = req.body;
 
@@ -61,4 +61,24 @@ router.post('/login', async (req, res) => {
   }
 });
 
+
+router.post('/logout', protect, async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    // Simpan token ke denylist
+    await InvalidToken.create({
+      token,
+
+      expireAt: new Date(decoded.exp * 1000), 
+    });
+
+    res.status(200).json({ message: 'Logout berhasil' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+});
+
 module.exports = router;
+
